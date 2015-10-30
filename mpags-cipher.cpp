dd462a7c5819752737412c74fadd3e9f61b61949
Elliot Reynolds <etjr92@gmail.com>
2015-10-30 10:56:02 +0000

    first commit of 2nd week

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,9 +1,70 @@
+// Include required libraries
 #include <iostream>
 #include <string>
+
+std::string transformChar(char in_char) // Function which converts an input char to a string
+{
+  std::string outString {""};
+  
+  if(isupper(in_char))
+    {
+      outString += in_char;
+      return outString;
+    }  
+      
+  if(islower(in_char))
+    {
+      outString += toupper(in_char);
+      return outString;
+    }
+      
+      if(in_char=='0')
+	{
+	  return "ZERO";
+	}
+      if(in_char=='1')
+	{
+	  return "ONE";
+	}
+      if(in_char=='2')
+	{
+	  return "TWO";
+	}
+      if(in_char=='3')
+	{
+	  return "THREE";
+	}
+      if(in_char=='4')
+	{
+	  return "FOUR";
+	}
+      if(in_char=='5')
+	{
+	  return "FIVE";
+	}
+      if(in_char=='6')
+	{
+	  return "6";
+	}
+      if(in_char=='7')
+	{
+	  return "SEVEN";
+	}
+      if(in_char=='8')
+	{
+	  return "EIGHT";
+	}
+      if(in_char=='9')
+	{
+	  return "NINE";
+	}
+
+      return "";
+}
+
 int main(int argc, char *argv[])
 {
-
-  for(int i=0; i<argc; i++)
+  for(int i=0; i<argc; i++) // Acts on command line arguments
     {
       const std::string argument {argv[i]};
       if(argument == "-h" || argument == "--help") std::cout << "Help info here" << std::endl;
@@ -24,55 +85,13 @@ int main(int argc, char *argv[])
   
   std::string in{""};
   char in_char{'x'};
-  while(std::cin >> in_char)
+  
+  while(std::cin >> in_char) // Builds output from input
     {
-      if(isupper(in_char)) in.push_back(in_char);
-      
-      if(islower(in_char)) in.push_back(toupper(in_char));
-      
-      if(in_char=='0')
-	{
-	  in += "ZERO";
-	}
-      if(in_char=='1')
-	{
-	  in += "ONE";
-	}
-      if(in_char=='2')
-	{
-	  in += "TWO";
-	}
-      if(in_char=='3')
-	{
-	  in += "THREE";
-	}
-      if(in_char=='4')
-	{
-	  in += "FOUR";
-	}
-      if(in_char=='5')
-	{
-	  in += "FIVE";
-	}
-      if(in_char=='6')
-	{
-	  in += "6";
-	}
-      if(in_char=='7')
-	{
-	  in += "SEVEN";
-	}
-      if(in_char=='8')
-	{
-	  in += "EIGHT";
-	}
-      if(in_char=='9')
-	{
-	  in += "NINE";
-	}
+      in += transformChar(in_char);
     }
   
-  std::cout<<in<<std::endl;
+  std::cout<<in<<std::endl; // Outputs desired output
 
   return 0;
 }
